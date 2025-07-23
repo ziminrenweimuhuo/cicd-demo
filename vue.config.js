5c17bd7cdc1256fcd770a484b1b7cccc05e362ab
mengweijie1 <mengweijie1@jd.com>
2025-07-23 10:52:39 +0800

    ci(deploy): 优化GitHub Pages部署流程并更新项目路径配置

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
+module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/dice-demo/'
-    : '/'
-})
+    ? '/cicd-demo/'
+    : '/',
+  outputDir: 'dist'
+}
